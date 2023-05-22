fd3220c3cea7af4ad2dbb5c3552e62494f7f7b0b
Jose Carrion <joseloc@gmail.com>
2023-05-22 18:18:03 -0500

    update themes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,15 @@
-function loadTheme(color)
-	color = color or "gruvbox"
-	vim.cmd.colorscheme(color)
+-- function loadTheme(color)
+-- 	color = color or "gruvbox"
+-- 	vim.cmd.colorscheme(color)
+--
+-- 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- 	--
+--  --  vim.o.background = "dark"
+-- end
+--
+-- -- loadTheme()
+-- loadTheme("tokyonight-night")
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
--- loadTheme()
-loadTheme("tokyonight-day")
+-- vim.cmd.colorscheme("tokyonight-night")
+vim.cmd[[colorscheme tokyonight-night]]
