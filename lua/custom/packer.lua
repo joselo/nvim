5aa72d590f1889ffc38327a75a32da7ba54f2713
Jose Carrion <joseloc@gmail.com>
2023-10-12 09:59:40 -0500

    disable neotree

--- lua/custom/packer.lua
+++ lua/custom/packer.lua
@@ -139,14 +139,14 @@ return require('packer').startup(function(use)
 
   -- Nvim-neo-tree
 
-  use { "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    }
-  }
+  -- use { "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "v3.x",
+  --   requires = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+  --     "MunifTanjim/nui.nvim",
+  --   }
+  -- }
 
   use = { 'junegunn/fzf', run = './install --bin', }
 end)
