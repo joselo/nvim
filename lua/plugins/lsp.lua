1a5df3f9e8603899bae1b9a18f45ff74be7b92f7
Jose Carrion <joseloc@gmail.com>
2024-04-19 21:19:03 -0500

    update files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,85 +1,35 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    local lexical_config = {
-      filetypes = { "elixir", "eelixir", "heex", "surface" },
-      cmd = { "/home/joselo/.lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
-      settings = {},
+    -- generic LSP settings
+    --
+    -- ---@usage disable automatic installation of servers
+    -- lvim.lsp.automatic_servers_installation = false
+    local lspconfig = require("lspconfig")
+
+    -- Neovim doesn't support snippets out of the box, so we need to mutate the
+    -- capabilities we send to the language server to let them know we want snippets.
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+
+    -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+    -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+    -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+    -- local opts = {} -- check the lspconfig documentation for a list of all possible options
+    -- require("lvim.lsp.manager").setup("pyright", opts)
+    -- Elixir LS
+    lspconfig.elixirls.setup {
+      cmd = { "/home/joselo/.elixir-ls/language_server.sh" },
+      -- on_attach = custom_attach, -- this may be required for extended functionalities of the LSP
+      capabilities = capabilities,
+      flags = {
+        debounce_text_changes = 150,
+      },
+      elixirLS = {
+        dialyzerEnabled = false,
+        fetchDeps = false,
+      };
     }
-
-    local custom_attach = function(client)
-      print("Lexical has started.")
-    end
-
-		local lspconfig = require("lspconfig")
-		local configs = require("lspconfig.configs")
-
-		if not configs.lexical then
-			configs.lexical = {
-				default_config = {
-					filetypes = lexical_config.filetypes,
-					cmd = lexical_config.cmd,
-					root_dir = function(fname)
-						return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-					end,
-					-- optional settings
-					settings = lexical_config.settings,
-				},
-			}
-		end
-
-		lspconfig.lexical.setup({
-			-- optional config
-			on_attach = custom_attach,
-		})
-
-    -- keymap
-    local keymap = vim.api.nvim_set_keymap
-
-    local opts = { noremap = true, silent = true }
-
-    keymap("", "<Space>", "<Nop>", opts)
-    keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-    keymap("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
-
-    ----------------------------------------
-
-    -- Global mappings.
-    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-    -- Use LspAttach autocommand to only map the following keys
-    -- after the language server attaches to the current buffer
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-      callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
-          vim.lsp.buf.format { async = true }
-        end, opts)
-      end,
-    })
   end
 }
