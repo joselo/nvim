4504973877e55641701fa67508881235f5c757e8
Jose Carrion <joseloc@gmail.com>
2024-04-24 21:52:00 -0500

    update libs

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -14,50 +14,49 @@ return {
     local cmp_action = lsp_zero.cmp_action()
 
     cmp.setup({
+      formatting = lsp_zero.cmp_format({details = true}),
+      mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = true,
+        },
+        ["<Down>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Wrap })
+          elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+            "i",
+            "s",
+          }),
+        ["<Up>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Wrap })
+          elseif require("luasnip").jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+            "i",
+            "s",
+          }),
+      },
+
       snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-          -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-          -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-          -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-          -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+          require('luasnip').lsp_expand(args.body)
         end,
       },
-      window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      }),
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-      }, {
-          { name = 'buffer' },
-        })
-
-      -- formatting = lsp_zero.cmp_format({details = true}),
-      -- mapping = cmp.mapping.preset.insert({
-      --   ['<C-Space>'] = cmp.mapping.complete(),
-      --   ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-      --   ['<C-d>'] = cmp.mapping.scroll_docs(4),
-      --   ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-      --   ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-      -- }),
-      -- snippet = {
-      --   expand = function(args)
-      --     require('luasnip').lsp_expand(args.body)
-      --   end,
-      -- },
     })
   end
 }
