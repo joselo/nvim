78d8de79504d152240bf29ed292176197ddb8681
joselo <joseloc@gmail.com>
2024-06-18 16:25:44 -0500

    fix plugins

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,18 +1,4 @@
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    enabled = false,
-  },
-
-  {
-    "echasnovski/mini.indentscope",
-    opts = {
-      draw = {
-        delay = 100,
-        animation = function()
-          return 20
-        end,
-      },
-    },
-  },
+  "lukas-reineke/indent-blankline.nvim",
+  enabled = false
 }
---
+++ lua/plugins/indentscope.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.indentscope",
+  opts = {
+    draw = {
+      delay = 100,
+      animation = function()
+        return 20
+      end,
+    },
+  }
+}
