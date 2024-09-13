23a5f9d3bca7edf4004c52aab14602cbcf18136a
joselo <joseloc@gmail.com>
2024-09-13 09:11:11 -0500

    add neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,5 +9,21 @@ return {
     },
     config = function ()
       vim.keymap.set('n', '-', '<Cmd>Neotree toggle<CR>')
+
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false,
+          },
+        },
+        buffers = { follow_current_file = { enable = true } },
+      })
     end
 }
